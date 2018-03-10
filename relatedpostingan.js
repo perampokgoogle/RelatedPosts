b0fb21da1d3725279a29b72b6818b10c415accc6
perampokgoogle <localhost367@gmail.com>
2018-03-11 04:56:04 +0700

    Create relatedpostingan.js

---
+++ relatedpostingan.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "https://www.bloggerszones.com/") {
+        window.location.href = "https://www.bloggerszones.com/"; }});
+//]]>
